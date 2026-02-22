40de0c4a7226f14fc4b39ff57c1f17725cf358d8
medmax <medmax5@gmail.com>
2026-02-22 13:47:27 +0100

    sanity

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -6,10 +6,7 @@ export default defineCliConfig({
     dataset: 'production'
   },
   deployment: {
-    /**
-     * Enable auto-updates for studios.
-     * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
-     */
+    appId: 'wpuzeb7l0dof558lrpk91ufp',
     autoUpdates: true,
   }
 })
